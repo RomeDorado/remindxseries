63e5001adc68f4120f2c4b7547af0b163390f451
Sachin Bhatnagar <sachinbhatnagar@Magrathea.local>
2016-10-06 15:32:33 +0530

    PersistentTest

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ agenda.on('ready', () => {
 				postback,
 				message
 			} = msg;
-			console.log(message);
+			console.log(msg);
 			if(postback && !postback.payload.includes("menu")) {
 				console.log("Reached here");
 					const {
