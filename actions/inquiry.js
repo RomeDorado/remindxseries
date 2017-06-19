9cc0d40cfa2cc105b7bc9848a93c424de92e4e2b
RomeDorado <romedorado@gmail.com>
2017-06-19 13:29:12 +0800

    inquiry

--- actions/inquiry.js
+++ actions/inquiry.js
@@ -1,7 +1,7 @@
 'use strict';
 const request = require('request');
 const {fetchEntity} = require('../utils');
-//const createResponse = require('../utils');
+const createResponse = require('../utils');
 const config = require('../config');
 const inquiry = ({sessionId, context, entities}) => {
   /*
@@ -44,53 +44,3 @@ const inquiry = ({sessionId, context, entities}) => {
 
 module.exports = inquiry;
 
-const createResponse = (intent, tvshow) => {
-  if(tvshow.Response === 'True') {
-    let {
-      Title,
-      Year,
-      Plot,
-      Director,
-      Actors,
-      Poster
-    } = tvshow;
-
-    switch(intent) {
-      case 'tvInfo' : {
-        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`;
-        return {
-          text: str,
-          image: Poster
-        }        
-      }
-
-      // case 'releaseYear' : {
-      //   let str = `${Title} was released in ${Year}.`;
-      //   return {
-      //     text: str,
-      //     image: null
-      //   }
-      // }
-      //
-      case 'director' : {
-        let str = `${Title} (${Year}) was directed by ${Director}.`;
-        return {
-          text: str,
-          image: null
-        }
-      }
-      //
-      // default: {
-      //   return {
-      //     text: "Always at your service :)",
-      //     image: null
-      //   }
-      // }
-    }
-  } else {
-    return {
-      text: "I don't seem to understand your question!",
-      image: null
-    }
-  }
-}
