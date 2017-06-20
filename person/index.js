44f7de2fd2a4f9f0a4297611a4d0c1c9f75b0d59
RomeDorado <romedorado@gmail.com>
2017-06-20 11:33:05 +0800

    ftext

--- person/index.js
+++ person/index.js
@@ -15,8 +15,7 @@ const createResponse = (intent, tvshow) => {
 
     switch(intent) {
       case 'tvInfo' : {          
-        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`;
-        f.text(sender, str);
+        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`;        
         return {
           text: str,
           image: Poster
