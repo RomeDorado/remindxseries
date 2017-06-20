d55806d3665b1d576de0477f2dc5b34fd59fdcb9
RomeDorado <romedorado@gmail.com>
2017-06-20 11:03:44 +0800

    hiwalay

--- omdb/index.js
+++ omdb/index.js
@@ -1,12 +1,18 @@
 'use strict';
 const request = require('request');
+const {fetchEntity} = require('../utils');
 const createResponse = require('../person');
 const config = require('../config');
-const getInfo = data => {
+const inquiry = ({sessionId, context, entities}) => {
+  /*
   let intent = data.entities.intent && data.entities.intent[0].value || 'tvInfo';
   let tvshow = data.entities.tvshow && data.entities.tvshow[0].value || null;
+  */
+  let intent = fetchEntity(entities, 'intent') || "tvInfo";
+  let tvshow = fetchEntity(entities, 'tvshow') || null;
   return new Promise((resolve, reject) => {
-    if(tvshow) {
+    if(intent && tvshow) {
+      console.log("intent is "+ intent +  " and tvshow is" +  tvshow);
       // Fetch data from OMDB
       request({
         uri: "https://www.omdbapi.com",
@@ -14,21 +20,27 @@ const getInfo = data => {
           t: tvshow,
           plot: 'short',
           r: 'json',
-          apiKey: config.OMDB_API_KEY
+          apiKey: '270b7488'
         },
-        method: 'GET'
+        method: 'GET'        
       }, (error, response, body) => {
+        console.log(response.statusCode);
+        console.log(JSON.parse(body));
         if(!error && response.statusCode === 200) {
+          
           resolve(createResponse(intent, JSON.parse(body)));
+          
         } else {
           reject(error);
         }
       });
 
     } else {
-      reject("Entities not found in omdb!");
+      reject("Entities not found in inquiry!");
     }
+    return resolve(context);
   });
 }
 
-module.exports = getInfo;
+module.exports = inquiry;
+
