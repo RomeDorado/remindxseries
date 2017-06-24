fb4b820f5b81720e9c341f36892da3bb49d801eb
RomeDorado <romedorado@gmail.com>
2017-06-24 21:37:18 +0800

    trial

--- server.js
+++ server.js
@@ -77,16 +77,12 @@ agenda.on('ready', () => {
 				// Run WIT Actions (Converse API)
 				wit.runActions(sessionId, messageTxt, context)
 				
-					.then(omdb)
-				.then(response => {					
-					f.txt(sender, response.text);
-					if(response.image) {
-						f.img(sender, response.image + "ITO ATA YUNG NULL");
-					}
-				})
-				.catch(error => console.log(error + "this is the error"));		
+					.then(ctx => {
+						// Delete session if the conversation is over
+						ctx.jobDone ? session.delete(sessionId) : session.update(sessionId, ctx);
+					})				
 					
-				
+					.catch(error => console.log(error));
 								
 				
 			}
