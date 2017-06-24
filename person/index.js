1dc6e75810352d1eeac71afcf55ef14b66517282
RomeDorado <romedorado@gmail.com>
2017-06-24 17:45:43 +0800

    trial

--- person/index.js
+++ person/index.js
@@ -14,31 +14,8 @@ const createResponse = (intent, tvshow, context) => {
     } = tvshow;
     
 
- send(request, response) {
-      const {sessionId, context, entities} = request;
-      const {text, quickreplies} = response;
-      return new Promise((resolve, reject) => {
-        let {fbid} = session.get(sessionId);
-        if(quickreplies) {
-          let buttons = quickreplies.map(title => {
-            return {
-              title,
-              content_type: "text",
-              payload: "null"
-            }
-          });
 
-          f.quick(fbid, {
-            text,
-            buttons
-          });
-        } else {
-          f.txt(fbid, text);
-        }
-        return resolve();
-      });
-    }
-
+    
 
     switch(intent) {
     
