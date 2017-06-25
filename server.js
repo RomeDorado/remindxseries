975ad9b01e31314c41397a5cefa9ab1eb0df9251
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-25 14:39:42 +0800

    asdf

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ agenda.on('ready', () => {
 					});
 			}
 
-			if((message && message.text) || (postback && postback.payload.includes("menu"))) {
+			if((message.text || (postback && postback.payload.includes("menu"))) {
 				// Process the message here
 				let sessionId = session.init(sender);
 				let {context} = session.get(sessionId);
